6fa75c29a73c48538630cfb0fd3362919e8033da
wookyung <wk.nam.dev@gmail.com>
2024-10-07 15:55:16 +0900

    배열의 정렬_24.10.07

---
+++ java_basic/chapter05/Sort.java
@@ -0,0 +1,11 @@
+package chapter05;
+
+import java.util.Arrays;
+
+public class Sort {
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 0, 1, 4};
+        Arrays.sort(arr);   // 배열 arr을 정렬
+        System.out.println(Arrays.toString(arr));
+    }
+}
